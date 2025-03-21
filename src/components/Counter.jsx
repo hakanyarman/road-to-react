44e8bd2f1cedf7c43a236c3aaa6b37a4a1741187
Hakan Yarman <104122864+hakanyarman@users.noreply.github.com>
2025-03-22 02:17:38 +0300

    conditional rendering

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,6 +4,7 @@ export const Counter = () => {
   const [counts, setCounts] = useState({ leftCount: 0, rightCount: 0 });
 
   const [allClicks, setAllClicks] = useState([]);
+  const [totalClicks, setTotalClicks] = useState(allClicks.length);
 
   const handleLeftClick = () => {
     setCounts({
@@ -11,33 +12,42 @@ export const Counter = () => {
       leftCount: counts.leftCount + 1,
     });
     setAllClicks(allClicks.concat("left"));
+    setTotalClicks(totalClicks + 1);
     // allClicks.concat("left") aslında yeni bir array. yani setAllClicks(newAllClicks) gibi bir şey yaptık aslında.
   };
 
-//   const handleRightClick = () => {
-//     setCounts({
-//       ...counts,
-//       rightCount: counts.rightCount + 1,
-//     });
-//     setAllClicks(allClicks.concat("right"))
-//   };
+  //   const handleRightClick = () => {
+  //     setCounts({
+  //       ...counts,
+  //       rightCount: counts.rightCount + 1,
+  //     });
+  //     setAllClicks(allClicks.concat("right"))
+  //   };
 
-// diğer yöntemimiz bu setAllClicks için. doğrudan state'in kendini değiştirmemeliyiz.
-const handleRightClick = () => {
+  // diğer yöntemimiz bu setAllClicks için. doğrudan state'in kendini değiştirmemeliyiz.
+  const handleRightClick = () => {
     setCounts({
       ...counts,
       rightCount: counts.rightCount + 1,
     });
-    setAllClicks(...allClicks.push("right"))
+    // setAllClicks(...allClicks.push("right"))
+    setAllClicks(allClicks.concat("right"));
+    setTotalClicks(totalClicks + 1);
   };
-
 
   return (
     <div>
       <button onClick={handleLeftClick}>left {counts.leftCount}</button>
       <button onClick={handleRightClick}>right {counts.rightCount}</button>
       <div>
-        <span>click logs: </span>{allClicks.join(" ")}
+        {allClicks.length ? (
+          <p>total click: {totalClicks}</p>
+        ) : (
+          <p>you havent clicked yet</p>
+        )}
+
+        <span>click logs: </span>
+        {allClicks.join(" ")}
       </div>
     </div>
   );
