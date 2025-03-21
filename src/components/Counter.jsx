c82bfa5192e19d2a28a25b2d43ab0ee9ab50038d
Hakan Yarman <104122864+hakanyarman@users.noreply.github.com>
2025-03-21 17:41:42 +0300

    click logs

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,11 +3,14 @@ import React, { useState } from "react";
 export const Counter = () => {
   const [counts, setCounts] = useState({ leftCount: 0, rightCount: 0 });
 
+  const [allClicks, setAllClicks] = useState([]);
+
   const handleLeftClick = () => {
     setCounts({
       ...counts,
       leftCount: counts.leftCount + 1,
     });
+    setAllClicks(allClicks.concat("left"));
   };
 
   const handleRightClick = () => {
@@ -15,12 +18,16 @@ export const Counter = () => {
       ...counts,
       rightCount: counts.rightCount + 1,
     });
+    setAllClicks(allClicks.concat("right"))
   };
 
   return (
     <div>
       <button onClick={handleLeftClick}>left {counts.leftCount}</button>
       <button onClick={handleRightClick}>right {counts.rightCount}</button>
+      <div>
+        <span>click logs: </span>{allClicks.join(" ")}
+      </div>
     </div>
   );
 };
